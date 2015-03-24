206a9a12d7491ddc67d0a98c8bb192ed14b8e265
root <root@huitres>
2015-03-24 15:45:01 +0100

    urls et templates

--- cuisine/settings.py
+++ cuisine/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recettes',
+    'debug_toolbar'
 )
 
 MIDDLEWARE_CLASSES = (
--- recettes/urls.py
+++ recettes/urls.py
@@ -1,1 +1,6 @@
-__author__ = 'root'
+from django.conf.urls import patterns, include, url
+from .views import index
+
+urlpatterns = patterns('',
+                       url(r'^$', index, name='index'),
+)
