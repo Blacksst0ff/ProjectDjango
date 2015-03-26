85b062006357ab272839c82b637e28d7d7c8d7d4
Bernier Kevin <bernier.kevin59@gmail.com>
2015-03-26 22:23:07 +0100

    Add Template HTML CSS JS
    Home page only

--- recettes/views.py
+++ recettes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse('bonjour mr_porc')
+    return render(request, 'recettes/index.html')
