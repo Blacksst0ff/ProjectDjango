ec6f30a2f7fe2fdaa1c99bb90dbe6711ab35c413
Bernier Kevin <bernier.kevin59@gmail.com>
2015-05-27 00:53:49 +0200

    Auth Incomplet

--- recettes/urls.py
+++ recettes/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
-from .views import index, consulter
+from .views import index, consulter, login, auth_view, logout, loggedin, invalid_login
 
 urlpatterns = patterns('',
                        url(r'^$', index, name='index'),
                        url(r'^consulter/$', consulter, name='consulter'),
+
+                       # USER PART
+                       url(r'^login/$', login, name='login'),
+                       url(r'^auth/$', auth_view, name='auth_view'),
+                       url(r'^logout/$', logout, name='logout'),
+                       url(r'^loggedin/$', loggedin, name='loggedin'),
+                       url(r'^invalid/$', invalid_login, name='invalid_login'),
 )
